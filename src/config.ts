ef36705494a0c3973ef320f25647b8533243400b
Oskar <oskar.wichtowski3@gmail.com>
2023-01-08 23:09:17 +0100

    remodel firebase

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  firebase: {
     apiKey: "AIzaSyBTtUSwmQu1OaAo0LfdDzjBFjpdIPo6u_U",
     authDomain: "skillbuddy-db.firebaseapp.com",
+    databaseURL: "https://skillbuddy-db-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "skillbuddy-db",
     storageBucket: "skillbuddy-db.appspot.com",
     messagingSenderId: "662476831015",
     appId: "1:662476831015:web:da3e6fa433b38d2c3bd102",
     measurementId: "G-3JZQVX92G4"
-  }
 };
+
